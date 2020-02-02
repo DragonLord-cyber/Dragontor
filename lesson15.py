1899adc481367ccdbe4287b98dc01b270de76b09
DragonLord-cyber <56875054+DragonLord-cyber@users.noreply.github.com>
2020-02-02 17:55:08 +0300

    Create lesson15.py

---
+++ lesson15.py
@@ -0,0 +1,43 @@
+# class Rest():
+#     def __init__(self, a, b):
+#         self.rest_name = a
+#         self.cuisine_type = b
+#     def describe_rest(self):
+#         print(self.rest_name)
+#         print(self.cuisine_type)
+#     def open_rest(self):
+#         print("Rest open")
+# r = Rest("Space", "American")
+# print(r.rest_name)
+# print(r.cuisine_type)
+# r.describe_rest()
+# r.open_rest()
+#
+# m = Rest("name1", "type2")
+# m.describe_rest()
+# z = Rest("name2", "type2")
+# z.describe_rest()
+# p = Rest("name3", "type3")
+# p.describe_rest()
+#
+#
+
+class User():
+    def __init__(self, first_name, last_name):
+        self.first_name
+        self.last_name
+        self.age = age
+        self.city = city
+    def describe_user(self):
+        print(self.first.name)
+        print(self.last.name)
+        print(self.age)
+        print(self.city)
+    def greet_user(self):
+        print("Hello , " , self.first.name)
+d = User("Sasha", "Semenov", 13, "Moscow")
+d.describe_user()
+p = User("Pasha", "Semenov", 13, "Moscow")
+p.describe_user()
+s = User("Dasha", "Semenov", 13, "Moscow")
+s.describe_user()
